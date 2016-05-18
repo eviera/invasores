93d853be4b253619c430d22b87d6ecef26a7eddd
eviera@asterix <eviera@asterix>
2016-05-18 01:20:27 -0300

    test movimiento

--- src/Invasores.kt
+++ src/Invasores.kt
@@ -1,23 +1,29 @@
-import org.newdawn.slick.AppGameContainer
-import org.newdawn.slick.BasicGame
-import org.newdawn.slick.GameContainer
-import org.newdawn.slick.Graphics
+import org.newdawn.slick.*
 
 class Invasores : BasicGame("Invasores") {
 
+    var nave :Image? = null
+    var x = 200f;
+
     override fun init(gc: GameContainer?) {
+        nave = Image("/resources/images/nave_64.png")
     }
 
     override fun update(gc: GameContainer?, delta: Int) {
+        val input = gc?.input
+        if (input?.isKeyDown(Input.KEY_LEFT)!!) { x-- }
+        if (input?.isKeyDown(Input.KEY_RIGHT)!!) { x++ }
+
     }
 
     override fun render(gc: GameContainer?, g: Graphics?) {
+        g?.drawImage(nave, x, 200f)
     }
 
 }
 
 fun main(args: Array<String>) {
     val appgc = AppGameContainer(Invasores())
-    appgc.setDisplayMode(640, 480, false)
+    appgc.setDisplayMode(800, 600, false)
     appgc.start()
 }
