027f9df440275a116235ef9b3e4184b8cbceeb29
eviera@sdd <eviera@sdd>
2016-06-08 19:50:21 -0300

    font

--- src/Const.kt
+++ src/Const.kt
@@ -1,6 +1,6 @@
 object Const {
-    val GAME_WIDTH = 1400
-    val GAME_HEIGHT = 900
+    val GAME_WIDTH = 1200
+    val GAME_HEIGHT = 600
     val SP_SIZE = 64f
     val PLAYER_START_X: Float = GAME_WIDTH / 2f
     val PLAYER_START_Y: Float = GAME_HEIGHT - SP_SIZE * 2
--- src/Manager.kt
+++ src/Manager.kt
@@ -1,9 +1,13 @@
 import java.util.*
 import Const.SP_SIZE
 import org.newdawn.slick.*
+import org.newdawn.slick.util.ResourceLoader
+import java.awt.Font
+import java.io.File
 
 object Manager {
 
+    val fontComputer24 = TrueTypeFont(Font.createFont(Font.TRUETYPE_FONT, ResourceLoader.getResourceAsStream("/resources/fonts/Computerfont.ttf")).deriveFont(24f), false)
 
     fun init(gc: GameContainer, player: Player, aliens: Array<Alien?>) {
         //Cargo la spritesheet
@@ -15,10 +19,10 @@ object Manager {
 
         //Cargo los aliens
         for (f in 0..2) {
-            val colDisp = if (f == 1 ) 1.5f else 1f
+            val colDisp = if (f == 1 ) 1.3f else 1f
             for (c in 0..7) {
                 val alienAnimation = Animation(arrayOf(sprites.getSprite(f * 2, 0), sprites.getSprite(f * 2 + 1, 0)), getRandomAnimationInterval())
-                val alien = Alien(SP_SIZE * colDisp + c * SP_SIZE * 2, SP_SIZE + SP_SIZE * f * 2f)
+                val alien = Alien(SP_SIZE * colDisp + c * SP_SIZE * 1.7f, SP_SIZE / 3 + SP_SIZE * f * 1.3f)
                 alien.init(alienAnimation)
                 aliens[f * 8 + c] = alien
             }
@@ -35,6 +39,7 @@ object Manager {
         //Scoreboard
         g.color = Color.white
         g.drawLine(0f, Const.GAME_HEIGHT - Const.SP_SIZE + 5, Const.GAME_WIDTH * 1f, Const.GAME_HEIGHT - Const.SP_SIZE + 5)
+        fontComputer24.drawString(0f, Const.GAME_HEIGHT - 50f, "POINTS")
 
 
 
