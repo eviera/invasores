336e301f667d41adf23068fa7d2f6a1c7040aedc
eviera@asterix <eviera@asterix>
2016-06-24 00:45:01 -0300

    alien destroyed

--- src/Alien.kt
+++ src/Alien.kt
@@ -5,6 +5,7 @@ import org.newdawn.slick.Graphics
 class Alien (x: Float, y: Float, var alienDisplacement: Float) : CollisionableRectangle(x, y, Const.SP_SIZE, Const.SP_SIZE){
 
     lateinit var sprite: Animation
+    var alive: Boolean = true
 
     fun init(sprite: Animation) {
         this.sprite = sprite
@@ -12,20 +13,25 @@ class Alien (x: Float, y: Float, var alienDisplacement: Float) : CollisionableRe
     }
 
     fun update(gc: GameContainer, delta: Int, alienDisplacement: Float) {
-        this.alienDisplacement = alienDisplacement
+        if (alive) {
+            this.alienDisplacement = alienDisplacement
+        }
     }
 
     fun render(gc: GameContainer, g: Graphics) {
-        x += alienDisplacement
-        sprite.draw(x , y)
+        if (alive) {
+            x += alienDisplacement
+            sprite.draw(x, y)
+        }
     }
 
     fun remove() {
         CollisionManager.removeAlien(this)
+        alive = false
     }
 
     override fun collisionWith(collisioned: CollisionableRectangle) {
-
+        remove()
     }
 
 }
