93e4d52b35312a15ea309af8163ef35ff5fef2e3
eviera@asterix <eviera@asterix>
2016-10-03 23:04:35 -0300

    game over background

--- src/net/eviera/invasores/state/GameOverState.kt
+++ src/net/eviera/invasores/state/GameOverState.kt
@@ -1,16 +1,19 @@
 package net.eviera.invasores.state
 
 import net.eviera.invasores.helper.Const
-import org.newdawn.slick.Color
-import org.newdawn.slick.GameContainer
-import org.newdawn.slick.Graphics
-import org.newdawn.slick.Input
+import net.eviera.invasores.helper.Helper
+import org.newdawn.slick.*
 import org.newdawn.slick.state.BasicGameState
 import org.newdawn.slick.state.StateBasedGame
 
 class GameOverState : BasicGameState() {
 
+    lateinit var backimg: Image
+    lateinit var fontComputer24: TrueTypeFont
+
     override fun init(gc: GameContainer?, game: StateBasedGame?) {
+        backimg = Image("/resources/images/gameover_background.png")
+        fontComputer24 = Helper.getComputerFont(Const.FONT_SIZE_24)
 
     }
 
@@ -26,9 +29,8 @@ class GameOverState : BasicGameState() {
 
     override fun render(gc: GameContainer?, game: StateBasedGame?, g: Graphics?) {
         if (gc == null || g == null) throw RuntimeException("Error de inicializacion")
-
+        g.drawImage(backimg, 0f, 0f)
         g.color = Color.white
-        g.drawString("Game Over", 100f, 100f)
 
 
     }
